0943e1d95a31c0acaae12f652b9af1f6364613b3
Lavreny87 <Lavr19871014@yandex.ru>
2019-01-22 16:38:51 +0300

    add git ignore and do all task in first chapter

---
+++ src/StudyJavaBook.java
@@ -0,0 +1,73 @@
+public class StudyJavaBook {
+    public static void main(String[] args) {
+//К. Сьерра, Б. Бейтс - Изучаем Java
+
+        // Магнитики с кодм
+ /*       System.out.println("Привнтиек как дела? Повторение мать учения!!!))" );
+        int x = 3;
+        while (x > 0){
+            if (x > 2){
+                System.out.print("a");
+                x = x-1;
+                System.out.print("-");
+            }
+            if (x == 2){
+                System.out.print("b c");
+                x = x-1;
+                System.out.print("-");
+            }
+            if (x == 1){
+                System.out.print("d");
+                x = x-1;
+            }
+
+        }*/
+ // бутылки пмва на столе.
+ /*   int beer = 99;
+    String name = "бутылок (бутылки)";
+        System.out.println(beer + " " + name + " пива на столе");
+        while (beer > 0){
+
+            //System.out.println(beer + " " + name + " бутылое пива на столе");
+           // System.out.println(beer + " " + name + " пива.");
+            System.out.println("Возьми одну");
+            System.out.println("Пусти по кругу");
+            beer = beer - 1;
+           // if (beer < 5 & beer > 1) {
+          //      name = "бутылки";
+          //  }
+            if (beer == 1){
+                name = "бутылка";
+            }
+            if (beer > 0){
+                System.out.println(beer + " " + name + " пива на столе");
+            }else {
+                System.out.println("Нет пива на столе");
+            }
+        }*/
+
+/* //Головоломка
+        int x = 0;
+        while (x < 4){
+            //x  = x - 1;
+            System.out.print("a");
+            if (x < 1){
+                System.out.print(" ");
+            }
+            System.out.print("n");
+           // x = x + 2;
+            if (x > 1){
+                System.out.print(" oyser");
+                x = x + 2;
+            }
+            if (x == 1) {
+                System.out.print("noise");
+            }
+            if (x < 1){
+                System.out.print("oise");
+            }
+            System.out.println(" ");
+            x = x + 1;
+        }*/
+    }
+}
